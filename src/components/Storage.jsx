9c09315e244b2a7ce688ce5d9ef98984529ed1b9
Trang <nmtrangg1999@gmail.com>
2021-10-26 17:41:15 +0700

    add active when click on file/folder

--- src/components/Storage.jsx
+++ src/components/Storage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import "./folder.css"
+import React, { useState } from 'react'
+import "./storage.css"
 import FolderIcon from '../img/folder.png';
 import ImageIcon from '../img/image.png';
 import Wrapper from './Wrapper';
 const Storage = ({ data, type }) => {
+    // const [selected, setSelected] = useState(false)
+    const [selectedItem, setSelectedItem] = useState(null)
+
+    const onClickedHandle = (index) => {
+        setSelectedItem(index)
+    }
 
     return (
         <Wrapper>
@@ -11,13 +17,21 @@ const Storage = ({ data, type }) => {
                 data.map((app, idx) => (
                     (type == "folder") ?
                         (
-                            <div className="folder-grid grid" key={idx}>
+                            <div
+                                className={"folder-grid grid " + (idx === selectedItem ? "selected" : null)}
+                                key={idx}
+                                onClick={() => onClickedHandle(idx)}
+                            >
                                 <img src={FolderIcon} alt="folder-icon" className="folder-icon" />
                                 {app.name}
                             </div>
                         ) :
                         (
-                            <div className="file-grid grid" key={idx}>
+                            <div
+                                className={"file-grid grid " + (idx === selectedItem ? "selected" : null)}
+                                key={idx}
+                                onClick={() => onClickedHandle(idx)}
+                            >
                                 <img src={ImageIcon} alt="file-icon" className="file-icon" />
                                 {app.name}
                             </div>
