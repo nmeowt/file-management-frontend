d2782fed8a1cd62b14d4e9274a8651d943731825
Trang <nmtrangg1999@gmail.com>
2022-01-07 17:27:10 +0700

    update api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,8 @@
 import consts from "../consts";
 import { api } from "../utils/helper";
+import Cookies from 'universal-cookie';
 
+const hourToExpire = 24
 export const Auth = {
     login(data, setError, setIsLogined) {
         return api(
@@ -10,11 +12,15 @@ export const Auth = {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             data
-        ).then((result) => {
-            if (result.status === 'false') {
-                setError(prev => ([...prev, result.message]));
+        ).then((response) => {
+            if (response.token === 'null') {
+                setError(prev => ([...prev, "wrong username or password"]));
                 setIsLogined(false);
             } else {
+                const cookies = new Cookies();
+                let d = new Date();
+                d.setTime(d.getTime() + (hourToExpire * 60 * 60* 1000));
+                cookies.set("token", response.token, {path: "/", expires: d});
                 setIsLogined(true);
                 setError([]);
             }
